673ab24c5ece833218b6a440de714eae9e56d5ad
Konrad <szymoniak.konrad@gmail.com>
2017-12-09 13:12:42 +0100

    extend solution for second puzzle

--- day9/python/stream.py
+++ day9/python/stream.py
@@ -5,22 +5,22 @@ import sys
 def removeGarbage(stream):
     hasGarbage = lambda s: s.find('<') > 0
 
-    #stream = removeDoubleMarks(stream)
     stream = stream.replace('!!', '')
-    #begin = findOpening(stream)
-    #end = findClosing(stream)
-    #cleared = stream[0:begin] + stream[end + 1:]
+    totalGarbageLength = 0
+
     while hasGarbage(stream):
         begin = stream.find('<')
         end = stream.find('>')
         while stream[end - 1] == '!' and stream[end - 2] != '!':
-            #stream = stream[0:begin] + stream[end + 1:]
             end = stream.find('>', end + 1)
 
+        garbage = stream[begin + 1:end]
+        marks = garbage.count('!')
+        totalGarbageLength += len(garbage) - marks * 2
         stream = stream[0:begin] + stream[end + 1:]
-        print(begin, end, len(stream))
 
     stream = stream.replace(',', '')
+    print('Removed garbage length:', totalGarbageLength)
     return stream
 
 def calculateScore(stream):
@@ -34,17 +34,9 @@ def calculateScore(stream):
             current -= 1
     return score
 
-#stream = '{<a>,<a>,<a>,<a>}'
-#stream = '{{{{{{},<!!a!>o!>!>,<!!!>"!>u!>,<e!!!>,<o"!!!!!!!>,<!>,<!!!>}>}}}}'
-
 if len(sys.argv) != 2:
     print('Provide input file as command line argument!')
 else:
     stream = open(sys.argv[1]).read()
-    #stream = '{{<!!>},{<!!>},{<!!>},{<!!>}}'
     stream = removeGarbage(stream)
-    print(calculateScore(stream))
-
-#stream = removeDoubleMarks(stream)
-#stream = removeGarbage(stream)
-#print(stream)
+    print('Score:', calculateScore(stream))
