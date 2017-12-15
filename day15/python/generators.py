6a7342cbc3b7d2f9ff9a5feed346f59e43a010dc
Konrad Szymoniak <kszymoniak@vewd.com>
2017-12-15 09:13:39 +0100

    cleanup solution

--- day15/python/generators.py
+++ day15/python/generators.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
 
-import itertools
-
 def generator1(startingValue, factor):
     previousValue = startingValue
     divisor = 2147483647
 
-    for _ in itertools.count():
+    while True:
         previousValue = (previousValue * factor) % divisor
         yield previousValue
 
-def generator2(startingValue, factor, multiples):
+def generator2(startingValue, factor, multiplesOf):
     previousValue = startingValue
     divisor = 2147483647
 
-    for _ in itertools.count():
+    while True:
         previousValue = (previousValue * factor) % divisor
-        if previousValue % multiples:
-            continue
-        else:
+        if not previousValue % multiplesOf:
             yield previousValue
 
 g_a1 = generator1(591, 16807)
 g_b1 = generator1(393, 48271)
-
-count = 0
-for _ in range(0, 40000000):
-    a, b = next(g_a1), next(g_b1)
-    count += (a & 0xFFFF == b & 0xFFFF)
+count = sum((next(g_a1) & 0xFFFF == next(g_b1) & 0xFFFF) for _ in range(40000000))
 print('Part 1:', count)
 
 g_a2 = generator2(591, 16807, 4)
 g_b2 = generator2(393, 48271, 8)
-
-count = 0
-for _ in range(0, 5000000):
-    a, b = next(g_a2), next(g_b2)
-    count += (a & 0xFFFF == b & 0xFFFF)
+count = sum((next(g_a2) & 0xFFFF == next(g_b2) & 0xFFFF) for _ in range(5000000))
 print('Part 2:', count)
 
