f17625df50a3d51177a7116df013b6be666e9a16
Konrad <szymoniak.konrad@gmail.com>
2017-12-16 14:53:47 +0100

    add looking for cycle to allow computing step 2

--- day16/python/dance.py
+++ day16/python/dance.py
@@ -2,10 +2,9 @@
 
 import sys
 
-#programs = ['a', 'b', 'c', 'd', 'e']
-
 def spin(input, size):
-    return input[-size:] + input[:-size]
+    input = input[-size:] + input[:-size]
+    return input
 
 def exchange(input, pos1, pos2):
     input[pos1], input[pos2] = input[pos2], input[pos1]
@@ -17,14 +16,8 @@ def partner(input, program1, program2):
     input[pos1], input[pos2] = input[pos2], input[pos1]
     return input
 
-#programs = ['a', 'b', 'c', 'd', 'e']
-#programs = spin(programs, 1)
-#programs = exchange(programs, 3, 4)
-#programs = partner(programs, 'e', 'b')
-#print(programs)
-
-programs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-#programs = ['a', 'b', 'c', 'd', 'e']
+programs = list('abcdefghijklmnop')
+starting = list(programs)
 
 if len(sys.argv) != 2:
     print('Provide input file as command line argument!')
@@ -35,40 +28,28 @@ else:
     for step in steps:
         if step[0] == 's': # spin
             size = int(step[1:])
-            #print('spin', size)
             ops.append(lambda prg, size = size: spin(prg, size))
         elif step[0] == 'x': # exchange
             values = step[1:]
             val1, val2 = int(values[:values.index('/')]), int(values[values.index('/') + 1:])
-            #print('exchange', val1, val2)
             ops.append(lambda prg, val1 = val1, val2 = val2: exchange(prg, val1, val2))
         elif step[0] == 'p': # partner
             values = step[1:]
             val1, val2 = values[:values.index('/')], values[values.index('/') + 1:]
-            #print('parnter', val1, val2)
             ops.append(lambda prg, val1 = val1, val2 = val2: partner(prg, val1, val2))
 
-    #for _ in range(1):
-    #    for step in steps:
-    #        if step[0] == 's': # spin
-    #            size = int(step[1:])
-    #            programs = spin(programs, size)
-    #        if step[0] == 'x': # exchange
-    #            values = step[1:]
-    #            val1, val2 = int(values[:values.index('/')]), int(values[values.index('/') + 1:])
-    #            exchange(programs, val1, val2)
-    #        if step[0] == 'p': # partner
-    #            values = step[1:]
-    #            val1, val2 = values[:values.index('/')], values[values.index('/') + 1:]
-    #            partner(programs, val1, val2)
-
-    for _ in range(10000):
+    seen = [''.join(programs)]
+    for i in range(1000000000):
         for op in ops:
             programs = op(programs)
-print(''.join(programs))
-    #opz = []
-    #for i in range(5):
-    #    opz.append(lambda i = i: print(i))
 
-    #for op in opz:
-    #    op()
+        if i == 0:
+            print('After first dance:', ''.join(programs))
+
+        if programs == starting: # found cycle
+            programs = seen[1000000000 % (i + 1)]
+            break
+        else:
+            seen.append(''.join(programs))
+
+print('After 1 bilion dances:', programs)
