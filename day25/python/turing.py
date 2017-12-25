8b87d9837365b4d9503483de61b107686230b1af
Konrad <szymoniak.konrad@gmail.com>
2017-12-25 11:52:12 +0100

    refactor solution

--- day25/python/turing.py
+++ day25/python/turing.py
@@ -4,50 +4,32 @@ tape = [0]
 position = 0
 state = 0
 
-def step(value, move, newState):
+def step(*arg):
     global tape
     global position
     global state
 
-    tape[position] = value
-    position += move
+    if tape[position] == 0:
+        tape[position] = arg[0][0]
+        position += arg[1][0]
+        state = arg[2][0]
+    elif tape[position] == 1:
+        tape[position] = arg[0][1]
+        position += arg[1][1]
+        state = arg[2][1]
+
     if position < 0:
         tape = [0] + tape
         position += 1
     elif position == len(tape):
         tape = tape + [0]
-    state = newState
 
-for _ in range(12134527):
-    if state == 0: # state A
-        if tape[position] == 0:
-            step(1, 1, 1)
-        elif tape[position] == 1:
-            step(0, -1, 2)
-    elif state == 1: # state B
-        if tape[position] == 0:
-            step(1, -1, 0)
-        elif tape[position] == 1:
-            step(1, 1, 2)
-    elif state == 2: # state C
-        if tape[position] == 0:
-            step(1, 1, 0)
-        elif tape[position] == 1:
-            step(0, -1, 3)
-    elif state == 3: # state D
-        if tape[position] == 0:
-            step(1, -1, 4)
-        elif tape[position] == 1:
-            step(1, -1, 2)
-    elif state == 4: # state E
-        if tape[position] == 0:
-            step(1, 1, 5)
-        elif tape[position] == 1:
-            step(1, 1, 0)
-    elif state == 5: # state F
-        if tape[position] == 0:
-            step(1, 1, 0)
-        elif tape[position] == 1:
-            step(1, 1, 4)
+for i in range(12134527):
+    if   state == 0: step((1, 0), (1, -1),  (1, 2)) # state A
+    elif state == 1: step((1, 1), (-1, 1),  (0, 2)) # state B
+    elif state == 2: step((1, 0), (1, -1),  (0, 3)) # state C
+    elif state == 3: step((1, 1), (-1, -1), (4, 2)) # state D
+    elif state == 4: step((1, 1), (1, 1),   (5, 0)) # state E
+    elif state == 5: step((1, 1), (1, 1),   (0, 4)) # state F
 
 print(tape.count(1))
