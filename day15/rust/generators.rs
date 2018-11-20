6be7c18ba2126ecc01efea63ef1d94256f23b2ee
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2018-11-20 22:37:04 +0100

    refactor solution

--- day15/rust/generators.rs
+++ day15/rust/generators.rs
@@ -1,14 +1,11 @@
 struct Generator {
     value: usize,
-    factor: usize
+    factor: usize,
 }
 
 impl Generator {
     fn new(value: usize, factor: usize) -> Generator {
-        Generator {
-            value,
-            factor
-        }
+        Generator { value, factor }
     }
 }
 
@@ -22,70 +19,25 @@ impl Iterator for Generator {
     }
 }
 
-struct Generator2 {
-    value: usize,
-    factor: usize,
-    divisor: usize
-}
-
-impl Generator2 {
-    fn new(value: usize, factor: usize, divisor: usize) -> Generator2 {
-        Generator2 {
-            value,
-            factor,
-            divisor
-        }
-    }
-}
-
-impl Iterator for Generator2 {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<usize> {
-        self.value = (self.value * self.factor) % 2147483647;
-        if self.value % self.divisor == 0 {
-            Some(self.value)
-        } else {
-            None
-        }
-    }
-}
-
 fn main() {
-    let mut generator1 = Generator::new(591, 16807);
-    let mut generator2 = Generator::new(393, 48271);
+    let generator_a = Generator::new(591, 16807);
+    let generator_b = Generator::new(393, 48271);
 
-    let mut count = 0;
-    for _ in 0..40_000_000 {
-        if (generator1.next().unwrap() & 0xFFFF) == (generator2.next().unwrap() & 0xFFFF) {
-            count += 1;
-        }
-    }
+    let count = generator_a
+        .zip(generator_b)
+        .take(40_000_000)
+        .filter(|&(a, b)| a & 0xFFFF == b & 0xFFFF)
+        .count();
     println!("{}", count);
 
-    count = 0;
-    let mut generator1 = Generator2::new(591, 16807, 4);
-    let mut generator2 = Generator2::new(393, 48271, 8);
-    for _ in 0..5_000_000 {
-        let mut value1;
-        let mut value2;
-        loop {
-            value1 = generator1.next();
-            if value1 != None {
-                break;
-            }
-        }
+    let generator_a = Generator::new(591, 16807);
+    let generator_b = Generator::new(393, 48271);
 
-        loop {
-            value2 = generator2.next();
-            if value2 != None {
-                break;
-            }
-        }
-
-        if (value1.unwrap() & 0xFFFF) == (value2.unwrap() & 0xFFFF) {
-            count += 1;
-        }
-    }
+    let count = generator_a
+        .filter(|a| a % 4 == 0)
+        .zip(generator_b.filter(|b| b % 8 == 0))
+        .take(5_000_000)
+        .filter(|&(a, b)| a & 0xFFFF == b & 0xFFFF)
+        .count();
     println!("{}", count);
 }
